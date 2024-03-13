cb408f8aa047112ef45bb5c31e0c51b2ae078f30
Kacper_Kotynia <smth@smth.smth>
2024-03-13 15:12:27 +0100

    Lab2 koniec chyba???

--- SchoolRegister.Model/DataModels/Student.cs
+++ SchoolRegister.Model/DataModels/Student.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel;
+using System.ComponentModel.Design;
 using System.Text.RegularExpressions;
 using SchoolRegister.Model.DataModels;
 
@@ -49,24 +50,26 @@ public class Student : User
         get;
     }*/
 
-    public double AverageGrade => Grades == null || Grades.Count == 0 ? 0.0d : Math.Round(Grades.Average(g => (int)global.GradeValue), 1);
+    public double AverageGrade => Grades == null || Grades.Count == 0 ? 0.0d : Math.Round(Grades.Average(g => (int)g.GradeValue), 1);
 
-    public IDictionary
+    public IDictionary<string, double> AverageGradePerSubject => Grades == null ? new Dictionary<string, double>() :
+        Grades.GroupBy(g => g.Subject.Name)
+        .Select(g => new { SubjectName = g.Key, AvgGrade = Math.Round(g.Average(avg => (int)avg.GradeValue), 1) })
+        .ToDictionary(avg => avg.SubjectName, avg => avg.AvgGrade);
 
+    public IDictionary<string, List<GradeScale>> GradesPerSubject => Grades == null ? new Dictionary<string, List<GradeScale>>() : Grades
+        .GroupBy(g => g.Subject.Name)
+        .Select(g => new { SubjectName = g.Key, GradeList = g.Select(x => x.GradeValue).ToList() })
+        .ToDictionary(x => x.SubjectName, x=> x.GradeList);
     public Student() : base()
     {
-        /*Group = new Group();
+        Group = new Group();
         GroupId = 0;
         Grades = new List<Grade>();
         Parent = new Parent();
         ParentId = 0;
-        AverageGrade = 0.0;
-        AverageGradePerSubject = new Dictionary<string,double>();
-        GradePerSubject = new Dictionary<string,List<GradeScale>>();*/
+        //AverageGrade = 0.0;
+        //AverageGradePerSubject = new Dictionary<string,double>();
+        //GradePerSubject = new Dictionary<string,List<GradeScale>>();
     }
-
-
-
-
-
 }
