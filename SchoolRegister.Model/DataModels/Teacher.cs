2a4c079dddbb90655eeb5c5704ab0899e59bc2e2
Kamil_Zimnota <jan@smth.store>
2024-03-13 10:00:21 +0100

    lab1 kontynuacja

---
+++ SchoolRegister.Model/DataModels/Teacher.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace SchoolRegister.Model.DataModels
+{
+    public class Teacher:User
+    {
+        public IList<Subject> Subjects {get;set;}
+        public string Title {get;set;} = null!;
+
+        public Teacher () 
+        {
+            Subjects = new List<Subject>();
+
+        }
+        
+
+    }
+}
