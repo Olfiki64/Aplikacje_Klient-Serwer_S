535888927fed1ca5953b9870d472c4f2fd88dd08
Milosz_Gradys <smth@smth.smth>
2024-03-13 18:48:19 +0100

    Lab2_1st_attempt

--- SchoolRegister.Model/DataModels/Group.cs
+++ SchoolRegister.Model/DataModels/Group.cs
@@ -1,15 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace SchoolRegister.Model.DataModels
 {
     public class Group
     {
+        [Key]
         public int Id {get; set;}
-        public int SubjectId {get; set;}
-        public IList<Student> Students {get; set;} = null!;
+
+        public string Name {get; set;} = null!;
+        public virtual IList<Student> Students {get; set;} = null!;
         public IList<SubjectGroup> SubjectGroups {get; set;} = null!;
         Group(){}
     }
