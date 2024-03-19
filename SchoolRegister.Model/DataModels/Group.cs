e4df6821be124aa3a56a2d4e43ccc3b3f72822d8
Dominik_Kupczyk <jaghvdytqxg@bguvtyczsaiq.czhjiba>
2024-03-20 00:35:09 +0100

    lab2.2

--- SchoolRegister.Model/DataModels/Group.cs
+++ SchoolRegister.Model/DataModels/Group.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,7 @@ namespace SchoolRegister.Model.DataModels
 {
     public class Group
     {
-    //[Key]
+    [Key]
         public int Id {get; set;}
 
         public string Name {get; set;} = null!;
