b51d8b1574cb26838693873148d04a842402d435
Dominik_Kupczyk <rf6vytgcabs@uvbcyha.asda>
2024-03-27 03:18:16 +0100

    lab2.4

---
+++ SchoolRegister.ViewModels/VM/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+namespace SchoolRegister.ViewModels.VM
+
+{
+    public class ErrorViewModel
+    {
+        
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
