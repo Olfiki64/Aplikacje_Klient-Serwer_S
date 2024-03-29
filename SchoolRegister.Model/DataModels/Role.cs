0448167630b1409195d602e293b8d70a45de36e2
Michał Lamorski <21290215+mlamorski@users.noreply.github.com>
2024-03-29 13:01:18 +0100

    Update Model classes

--- SchoolRegister.Model/DataModels/Role.cs
+++ SchoolRegister.Model/DataModels/Role.cs
@@ -4,7 +4,7 @@ namespace SchoolRegister.Model.DataModels;
 
 public class Role : IdentityRole<int>
 {
-    public RoleValue RoleValue { get; set; }
+    public virtual RoleValue RoleValue { get; set; }
     public Role() { }
     public Role(string name, RoleValue roleValue)
     {
