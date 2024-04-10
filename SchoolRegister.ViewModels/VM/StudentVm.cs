d08697f692d4ca7364ac4e03a3c9fe2d58fa34d4
Patryk_Krol <smth@smth.smth>
2024-04-10 18:49:50 +0200

    nois

---
+++ SchoolRegister.ViewModels/VM/StudentVm.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolRegister.ViewModels.VM
+{
+    public class StudentVm
+    {
+        public object ParentName { get; set; }
+        public object GroupName { get; set; }
+    }
+}
