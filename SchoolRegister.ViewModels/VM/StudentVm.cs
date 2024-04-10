7715188b9a77afca049f70e2b1debc3e6f792e3e
Milosz_Gradys <smth@smth.smth>
2024-04-10 18:45:13 +0200

    Lab3_Up_to_task_4_not_included

--- SchoolRegister.ViewModels/VM/StudentVm.cs
+++ SchoolRegister.ViewModels/VM/StudentVm.cs
@@ -1,12 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace SchoolRegister.ViewModels.VM
 {
     public class StudentVm
     {
-        
+        public string GroupName { get; set; } = null!;
+        public string ParentName { get; set; } = null!;
     }
 }
