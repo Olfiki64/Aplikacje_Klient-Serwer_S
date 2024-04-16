1ba94842b9f84f0d5f2f4337cd10c599e9f82868
Adrian Drozdzowski <ad2137@adrian.com>
2024-04-16 13:33:52 +0200

    Lab3_2

---
+++ SchoolRegister.ViewModels/VM/StudentVm.cs
@@ -0,0 +1,14 @@
+namespace SchoolRegister.ViewModels.VM;
+
+public class StudentVm
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string? ParentName { get; set; }
+    public string? ParentEmail { get; set; }
+    public string? ParentPhoneNumber { get; set; }
+    public int GroupId { get; set; }
+    public GroupVm Group { get; set; } = null!;
+    public object GroupName { get; set; } = null!;
+}
