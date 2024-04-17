26faca4869dbf31877b4630e29da71b99205f5d3
Kacper Kotynia <km@km.pl>
2024-04-17 10:08:14 +0200

    Lab33

--- SchoolRegister.ViewModels/VM/StudentVm.cs
+++ SchoolRegister.ViewModels/VM/StudentVm.cs
@@ -1,46 +1,16 @@
-namespace SchoolRegister.ViewModels.VM;
-
-public class StudentVm : UserVm
-{  
-    public virtual GroupVm Group
-    {
-        get;
-        set;
-    }=null!;
-
-    public int? GroupId
-    {
-        get;
-        set;
-    }
-
-    public virtual IList<GradeVm> Grades
-    {
-        get;
-        set;
-    }=null!;
-
-    public virtual ParentVm Parent
-    {
-        get;
-        set;
-    }=null!;
-
-    public int? ParentId
-    {
-        get;
-        set;
-    }
-
-    public double AverageGrade => Grades == null || Grades.Count == 0 ? 0.0d : Math.Round(Grades.Average(g => (int)g.GradeValue), 1);
-
-    public IDictionary<string, double> AverageGradePerSubject => Grades == null ? new Dictionary<string, double>() :
-        Grades.GroupBy(g => g.Subject.Name)
-        .Select(g => new { SubjectName = g.Key, AvgGrade = Math.Round(g.Average(avg => (int)avg.GradeValue), 1) })
-        .ToDictionary(avg => avg.SubjectName, avg => avg.AvgGrade);
-
-    public IDictionary<string, List<GradeScale>> GradesPerSubject => Grades == null ? new Dictionary<string, List<GradeScale>>() : Grades
-        .GroupBy(g => g.Subject.Name)
-        .Select(g => new { SubjectName = g.Key, GradeList = g.Select(x => x.GradeValue).ToList() })
-        .ToDictionary(x => x.SubjectName, x=> x.GradeList);
+namespace SchoolRegister.ViewModels.VM
+{
+ public class StudentVm
+ {
+ public int Id { get; set; }
+ public string FirstName { get; set; } = null!;
+ public string LastName { get; set; } = null!;
+ public string ParentName { get; set; } = null!;
+ public int ParentId { get; set; }
+ public string GroupName { get; set; } = null!;
+ public double AverageGrade { get; set; }
+ public string UserName { get; set; } = null!;
+ public IDictionary<string, double> AverageGradePerSubject { get; set; } = null!;
+ public IDictionary<string, List<GradeScale>> GradesPerSubject { get; set; } = null!;
+ }
 }
--- SchoolRegister.ViewModels/VM/UserVm.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-namespace SchoolRegister.ViewModels.VM;
-public class UserVm : IdentityUser<int>
-{
-public string FirstName { get; set; } = null!;
-public string LastName { get; set; } = null!;
-public DateTime RegistrationDate { get; set; } = DateTime.Now;
-
-}
